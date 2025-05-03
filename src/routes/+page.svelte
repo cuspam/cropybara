e2b99f07f9a11f6d13e4cc03223a18617190b840
aaa4xu <aaa4xu@gmail.com>
2025-05-03 17:31:46 +0700

    🎉 Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { setLocale } from '$lib/paraglide/runtime';
+  import { m } from '$lib/paraglide/messages.js';
+</script>
+
+<h1>{m.hello_world({ name: 'SvelteKit User' })}</h1>
+<div>
+  <button onclick={() => setLocale('en')}>en</button>
+  <button onclick={() => setLocale('ru')}>ru</button>
+</div>
