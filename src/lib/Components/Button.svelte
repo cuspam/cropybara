3c583d96ae1e1833a66825c137a3dfcbffe6c9c2
aaa4xu <aaa4xu@gmail.com>
2025-05-04 14:06:54 +0700

    ♻️ Use SCSS mixin for button styles

--- src/lib/Components/Button.svelte
+++ src/lib/Components/Button.svelte
@@ -10,37 +10,9 @@
 </button>
 
 <style lang="scss">
+  @use '../../style/mixins.scss';
+
   button {
-    font-family: inherit;
-    display: inline-flex;
-    border-radius: var(--input-border-radius);
-    padding: var(--input-padding-vertical) var(--input-padding-horizontal);
-    background: transparent;
-    font-size: 1em;
-    font-weight: normal;
-    color: var(--color-text);
-    border: 1px solid var(--color-border);
-    user-select: none;
-  }
-
-  button:disabled {
-    border-color: var(--color-border-disabled);
-    color: var(--color-text-muted);
-  }
-
-  button:hover:not(:disabled),
-  button:focus {
-    border-color: var(--color-border-focus);
-    color: var(--color-text-active);
-  }
-
-  button:active:not(:disabled) {
-    background: rgba(0, 0, 0, 0.5);
-    border-color: var(--color-accent);
-  }
-
-  button:focus {
-    outline: none;
-    border-color: var(--color-accent);
+    @include mixins.button;
   }
 </style>
