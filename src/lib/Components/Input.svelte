532b865737ed7dd9a23b0a66a23df24e5af55e28
aaa4xu <aaa4xu@gmail.com>
2025-05-04 11:51:54 +0700

    ✨ Input component

---
+++ src/lib/Components/Input.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import type { HTMLInputAttributes } from 'svelte/elements';
+
+  const { ...rest }: HTMLInputAttributes = $props();
+</script>
+
+<input {...rest} />
+
+<style lang="scss">
+  input {
+    font-family: inherit;
+    border-radius: var(--input-border-radius);
+    padding: var(--input-padding-vertical) var(--input-padding-horizontal);
+    background: rgba(0, 0, 0, 0.5);
+    font-size: 1rem;
+    font-weight: normal;
+    color: var(--color-text);
+    border: 1px solid var(--color-border);
+  }
+
+  input:disabled {
+    color: var(--color-text-muted);
+    border-color: var(--color-border-disabled);
+  }
+
+  input:focus {
+    outline: none;
+    border-color: var(--color-accent);
+    color: var(--color-text-active);
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
   import { setLocale } from '$lib/paraglide/runtime';
   import { m } from '$lib/paraglide/messages.js';
-  import Button from '$lib/elements/Button.svelte';
+  import Input from '$lib/Components/Input.svelte';
+  import Button from '$lib/Components/Button.svelte';
 </script>
 
 <div>
   <Button onclick={() => setLocale('en')}>{m.test_english_btn()}</Button>
   <Button onclick={() => setLocale('ru')}>{m.test_russian_btn()}</Button>
   <Button disabled>Disabled</Button>
+</div>
+
+<div>
+  <Input type="number" />
+  <Input type="text" disabled value="some value" />
 </div>
