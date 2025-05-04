26c6be8a69e5a95eafb3b4e49a6c7d1421e1ad28
aaa4xu <aaa4xu@gmail.com>
2025-05-04 14:09:31 +0700

    ✨ FileInputButton component

---
+++ src/lib/Components/FileInputButton.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import FileInput from '$lib/Components/FileInput.svelte';
+  import type { Snippet } from 'svelte';
+  import type { HTMLInputAttributes } from 'svelte/elements';
+
+  type Props = Omit<HTMLInputAttributes, 'type'> & {
+    onFiles: (files: File[]) => void;
+    children: Snippet;
+  };
+
+  const { children, disabled, ...rest }: Props = $props();
+</script>
+
+<label class:disabled>
+  {@render children()}
+  <FileInput {disabled} {...rest} />
+</label>
+
+<style lang="scss">
+  @use '../../style/mixins.scss';
+
+  label {
+    @include mixins.button;
+  }
+</style>
