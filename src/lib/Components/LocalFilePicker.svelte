0e383b1f1970e7216b076dbb93c3e97cd7f1310f
aaa4xu <aaa4xu@gmail.com>
2025-05-05 18:34:10 +0700

    ✨ LocalFilePicker component

---
+++ src/lib/Components/LocalFilePicker.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import DirectoryPickerButton from '$lib/Components/FilePicker/DirectoryPickerButton.svelte';
+  import DragAndDropHandler from '$lib/Components/FilePicker/DragAndDropHandler.svelte';
+  import ClipboardPasteButton from '$lib/Components/FilePicker/ClipboardPasteButton.svelte';
+  import ZipArchivePickerButton from '$lib/Components/FilePicker/ZipArchivePickerButton.svelte';
+  import ImagesPickerButton from '$lib/Components/FilePicker/ImagesPickerButton.svelte';
+  import ClipboardPasteHandler from '$lib/Components/FilePicker/ClipboardPasteHandler.svelte';
+  import type { LocalFilesPickerProps } from '$lib/LocalFilesPickerProps';
+  import { m } from '$lib/paraglide/messages.js';
+
+  const { ...rest }: LocalFilesPickerProps = $props();
+</script>
+
+<section>
+  <h1>{m.Picker_LocalFilePicker_Header()}</h1>
+
+  <div>
+    <ImagesPickerButton {...rest} />
+    <DirectoryPickerButton {...rest} />
+    <ZipArchivePickerButton {...rest} />
+    <ClipboardPasteButton {...rest} />
+    <ClipboardPasteHandler {...rest} />
+    <DragAndDropHandler {...rest} />
+  </div>
+</section>
+
+<style lang="scss">
+  h1 {
+    text-align: center;
+    margin-top: 0;
+  }
+
+  div {
+    display: flex;
+    flex-direction: row;
+    gap: 0.5em;
+    justify-content: center;
+  }
+</style>
