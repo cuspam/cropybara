a0f549a4047fdaa2cae1ee9e1424a9dfdb512da6
aaa4xu <aaa4xu@gmail.com>
2025-05-05 18:31:16 +0700

    ✨ LocaleSwitcher component

---
+++ src/lib/Components/LocaleSwitcher.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { m } from '$lib/paraglide/messages.js';
+  import { getLocale, setLocale, locales } from '$lib/paraglide/runtime';
+</script>
+
+{#each locales as locale (locale)}
+  {#if getLocale() !== locale}
+    <a
+      href={`#locale-${locale}`}
+      onclick={(e) => {
+        e.preventDefault();
+        setLocale(locale);
+      }}
+      >{`LocaleSwitcher_Locale_${locale}` in m ? m[`LocaleSwitcher_Locale_${locale}`]() : locale}</a
+    >
+  {/if}
+{/each}
