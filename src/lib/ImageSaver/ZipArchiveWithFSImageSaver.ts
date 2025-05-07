3fd449d259c44e60e2d854f53a0e3bd2d2af9139
aaa4xu <aaa4xu@gmail.com>
2025-05-07 21:17:51 +0700

    ♻️ ZipArchiveWithFSImageSaver: Refactor saving logic to use streaming API

--- src/lib/ImageSaver/ZipArchiveWithFSImageSaver.ts
+++ src/lib/ImageSaver/ZipArchiveWithFSImageSaver.ts
@@ -33,23 +33,21 @@ export class ZipArchiveWithFSImageSaver implements ImagesSaver {
       onprogress?.();
     }
 
-    const content = await zip.generateAsync({ type: 'blob' });
+    const stream = zip.generateInternalStream({
+      type: 'arraybuffer',
+      streamFiles: true,
+    });
 
-    const readableStream = content.stream();
-
-    // more optimized pipe version
-    // (Safari may have pipeTo but it's useless without the WritableStream)
-    if (window.WritableStream && readableStream.pipeTo) {
-      return readableStream.pipeTo(fileStream).then(() => console.log('done writing'));
-    }
-
-    // Write (pipe) manually
-    const writer = fileStream.getWriter();
-
-    const reader = readableStream.getReader();
-    const pump: () => Promise<void> = () =>
-      reader.read().then((res) => (res.done ? writer.close() : writer.write(res.value).then(pump)));
-
-    pump();
+    await new Promise<void>((resolve, reject) => {
+      stream.on('data', (chunk) => {
+        fileStream.write(chunk);
+      });
+      stream.on('end', () => {
+        fileStream.close();
+        resolve();
+      });
+      stream.on('error', reject);
+      stream.resume();
+    });
   }
 }
