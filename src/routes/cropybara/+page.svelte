1466341344d06960126cc6e19c0ee605ba6d9ccd
aaa4xu <aaa4xu@gmail.com>
2025-05-08 09:42:32 +0700

    🚀 cropybara.app

---
+++ src/routes/cropybara/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    // Redirect for old clients
+    window.location.href = 'https://cropybara.app/';
+  });
+</script>
--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-let basePath = '';
-
-// noinspection JSUnresolvedReference
-if (process.env.GITHUB_ACTIONS === 'true') {
-  // noinspection JSUnresolvedReference
-  const githubRepo = process.env.GITHUB_REPOSITORY; // Format: 'owner/repo-name'
-
-  if (githubRepo) {
-    const repoName = githubRepo.split('/')[1]; // Extract 'repo-name'
-    if (repoName) {
-      basePath = `/${repoName}`; // Set base path like '/repo-name'
-      console.log(`GitHub Actions detected: Setting base path to ${basePath}`);
-    }
-  }
-}
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://svelte.dev/docs/kit/integrations
@@ -28,9 +12,6 @@ const config = {
     adapter: adapter({
       fallback: 'index.html',
     }),
-    paths: {
-      base: basePath,
-    },
   },
 };
 
