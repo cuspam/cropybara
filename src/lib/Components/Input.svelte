be965f017d7b8adb699c008da038a06f60cbe2e6
aaa4xu <aaa4xu@gmail.com>
2025-05-08 16:38:35 +0700

    ♻️ Add reusable input mixin in SCSS and refactor input styles

--- src/lib/Components/Input.svelte
+++ src/lib/Components/Input.svelte
@@ -7,25 +7,9 @@
 <input bind:value {...rest} />
 
 <style lang="scss">
+  @use '../../style/mixins.scss';
+
   input {
-    font-family: inherit;
-    border-radius: var(--input-border-radius);
-    padding: var(--input-padding-vertical) var(--input-padding-horizontal);
-    background: var(--input-background);
-    font-size: 1em;
-    font-weight: normal;
-    color: var(--color-text);
-    border: 1px solid var(--color-border);
-  }
-
-  input:disabled {
-    color: var(--color-text-muted);
-    border-color: var(--color-border-disabled);
-  }
-
-  input:focus {
-    outline: none;
-    border-color: var(--color-accent);
-    color: var(--color-text-active);
+    @include mixins.input;
   }
 </style>
