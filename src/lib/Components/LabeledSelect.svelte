c8b7be2b7836e9d16684d2fff38e25c362733c3b
aaa4xu <aaa4xu@gmail.com>
2025-05-08 16:45:59 +0700

    ✨ LabeledSelect component

---
+++ src/lib/Components/LabeledSelect.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import Select from '$lib/Components/Select.svelte';
+  import type { HTMLSelectAttributes } from 'svelte/elements';
+  import type { Snippet } from 'svelte';
+
+  type Props = {
+    children: Snippet;
+    label: Snippet;
+  } & HTMLSelectAttributes;
+  let { value = $bindable(), children, label, ...rest }: Props = $props();
+</script>
+
+<label>
+  <span>{@render label()}:</span>
+  <Select bind:value {...rest}>
+    {@render children()}
+  </Select>
+</label>
+
+<style lang="scss">
+  label {
+    display: flex;
+    flex-direction: column;
+    user-select: none;
+  }
+
+  span {
+    padding-bottom: 0.3em;
+  }
+</style>
