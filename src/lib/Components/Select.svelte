5582cb93fc0adde3b2ac9d2a1680882b7fec614d
aaa4xu <aaa4xu@gmail.com>
2025-05-08 16:39:14 +0700

    ✨ Select component

---
+++ src/lib/Components/Select.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import type { HTMLSelectAttributes } from 'svelte/elements';
+  import type { Snippet } from 'svelte';
+
+  type Props = {
+    children: Snippet;
+  } & HTMLSelectAttributes;
+  let { value = $bindable(), children, ...rest }: Props = $props();
+</script>
+
+<select bind:value {...rest}>
+  {@render children()}
+</select>
+
+<style lang="scss">
+  @use '../../style/mixins.scss';
+
+  select {
+    @include mixins.input;
+  }
+</style>
