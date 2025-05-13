38e4fc2c90af3ce93b0160be144a95acfa493086
aaa4xu <aaa4xu@gmail.com>
2025-05-13 12:48:03 +0700

    🧑‍💻 Enable source maps in Vite build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
       strategy: ['localStorage', 'preferredLanguage', 'baseLocale'],
     }),
   ],
+  build: {
+    sourcemap: true,
+  },
   worker: {
     format: 'es',
   },
