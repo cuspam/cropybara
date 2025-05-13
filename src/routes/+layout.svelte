451758c3fee203f945a1abf37e447bbe1d350724
aaa4xu <aaa4xu@gmail.com>
2025-05-13 12:48:33 +0700

    📈 Add PostHog analytics integration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,19 @@
   import ProgressBar from '$lib/Components/ProgressBar.svelte';
   import Alerts from '$lib/Components/Alerts.svelte';
   import Analytics from '$lib/Components/Analytics.svelte';
+  import posthog from 'posthog-js';
+  import { browser } from '$app/environment';
+  import { onMount } from 'svelte';
+  import * as env from '$env/static/public';
+
+  onMount(() => {
+    if (browser && typeof env['PUBLIC_POSTHOG_KEY'] === 'string') {
+      posthog.init(env['PUBLIC_POSTHOG_KEY'], {
+        api_host: 'https://eu.i.posthog.com',
+        person_profiles: 'always',
+      });
+    }
+  });
 </script>
 
 <Analytics />
